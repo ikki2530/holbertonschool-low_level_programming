e92715b7a2b8e4fa360423880c187b43428ffe3c
Diego Gómez <dagomez2530@gmail.com>
2020-02-05 23:50:45 +0000

    Changes line 17 and 18, reduce number of characters on 6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@ int main(void)
 {
 char sizeChar;
 int sizeInt;
-long int sizeLongInt;
-long long int sizeLongLongInt;
+long int sizeLgInt;
+long long int size;
 float sizeFloat;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(sizeChar));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(sizeInt));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sizeLongInt));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(sizeLongLongInt));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(sizeFloat));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sizeLgInt));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(size));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(sizeFloat));
 return (0);
 }
