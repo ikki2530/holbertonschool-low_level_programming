7b52acbbb42fbce3f4eac0aa7d551f84a5fd9f70
Diego Gómez <dagomez2530@gmail.com>
2020-02-05 23:53:44 +0000

    Changes to 6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(sizeChar));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(sizeInt));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sizeLgInt));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(size));
-printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(sizeFloat));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(sizeFloat));
 return (0);
 }
