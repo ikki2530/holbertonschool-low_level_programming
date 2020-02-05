a7056ec3d417feb3ec64e6d3ed3b17683624bd6b
Diego Gómez <dagomez2530@gmail.com>
2020-02-05 22:28:46 +0000

    Changes to 4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-  *Return: always 0
+ * main - Prints hello world
  *
- * main: function for entry point
- *
+ * Return: return 0
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
