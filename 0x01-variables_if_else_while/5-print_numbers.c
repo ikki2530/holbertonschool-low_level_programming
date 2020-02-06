19f68251190c505d89570355d88d2a08df2cbbda
Diego Gómez <dagomez2530@gmail.com>
2020-02-06 21:48:25 +0000

    Add 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet except q, e
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
