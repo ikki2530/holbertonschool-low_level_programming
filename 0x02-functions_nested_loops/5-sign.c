487de612189de9ecfafff4d21b0e4996f1d97374
Diego Gómez <dagomez2530@gmail.com>
2020-02-10 23:06:02 +0000

    Add 5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: Number to evaluate
+ * Return: 1 if n is greater than 0, 0 if n is equal to 0, -1 in other cases
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+/**
+ * main - Checks the print_sign function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
