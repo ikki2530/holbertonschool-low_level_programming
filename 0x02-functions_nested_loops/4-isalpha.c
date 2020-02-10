e706801a18b1158d9af07f2d150c7381e181b216
Diego Gómez <dagomez2530@gmail.com>
2020-02-10 22:29:24 +0000

    Add 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * _isalpha - Checks for alphabetic characters
+ * @c: Letter to evaluted
+ * Return: 1 if c is a letter, lowercase or uppercase. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * main - Checks the _isalpha function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,4 +2,5 @@
 
 0-holberton.c prints Holberton,
 1-alphabet.c prints the alphabet, in lowercase,
-2-print_alphabet_x10.c prints alphabet 10 times
+2-print_alphabet_x10.c prints alphabet 10 times,
+3-islower.c prints checks for lowercase character
