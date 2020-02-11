db363f316d809911ae372d01e0276d8394ce2628
Diego Gómez <dagomez2530@gmail.com>
2020-02-11 15:13:56 +0000

    Adding 0-holberton.c file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	int i = 0;
+	char st[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+
+	while (i < 9)
+	{
+		_putchar(st[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
