b028ce968ea1b1c08834f2627766bc88ca2564fb
Diego Gómez <dagomez2530@gmail.com>
2020-02-11 15:35:30 +0000

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - Add 2 numbers
+ * @a: Num to add
+ * @b: Num to add
+ * Return: return the result of the file
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
