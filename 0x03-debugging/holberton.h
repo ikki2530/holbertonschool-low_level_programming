33522d3012bb31eb0ab0b0509dcbfd4e0e57f504
Diego Gómez <dagomez2530@gmail.com>
2020-02-13 02:03:57 +0000

    Add 3-print_remaining_days.c file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -8,4 +8,7 @@ void positive_or_negative(int i);
 /* this prototype has to be declared in a header file */
 int largest_number(int a, int b, int c);
 
+/* this prototype has to be declared in a header file */
+void print_remaining_days(int month, int day, int year);
+
 #endif /* HOLBERTON_H */
