cf3e1c8695784a64dd87e6f5580f5eab21197ed2
Diego Gómez <dagomez2530@gmail.com>
2020-02-17 20:47:53 +0000

    Add 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string to reverse
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	i = 0;
+
+	for (i = count - 1; i >=  0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
