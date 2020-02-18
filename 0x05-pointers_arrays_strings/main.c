6f19859569b9b82e6d7b8d5f3eba691727cd6070
Diego Gómez <dagomez2530@gmail.com>
2020-02-18 17:30:51 +0000

    Changes to 8-print_array.c file

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -7,9 +7,13 @@
  */
 int main(void)
 {
-	char *str;
+	int array[5];
 
-	str = "01234567890";
-	puts_half(str);
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	print_array(array, 3);
 	return (0);
 }
