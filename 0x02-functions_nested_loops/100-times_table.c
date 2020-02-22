dc46ed734489db5e1158e8ea1926b015dc14b786
Diego Gómez <dagomez2530@gmail.com>
2020-02-22 19:08:04 +0000

    Add 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,9 +7,9 @@
  */
 void print_times_table(int n)
 {
-	if (n >= 0 || n <= 15)
+	if (n >= 0 && n <= 15)
 	{
-		int i, j, val, val2;
+		int i, j, val, val2, mid;
 		char ch;
 
 		for (i = 0; i <= n; i++)
@@ -26,19 +26,38 @@ void print_times_table(int n)
 					{
 						_putchar(',');
 						_putchar(' ');
-						
 						if (val2 < 10)
+						{
+							_putchar(' ');
+							_putchar(' ');
+						}
+						else
 							_putchar(' ');
 					}
 				}
 				else
 				{
-					_putchar((val / 10) + '0');
-					_putchar((val % 10) + '0');
+					if (val < 100)
+					{
+						_putchar((val / 10) + '0');
+						_putchar((val % 10) + '0');
+					}
+					else
+					{
+						mid = (val / 10) % 10;
+						_putchar((val / 100) + '0');
+						_putchar(mid + '0');
+						_putchar((val % 10) + '0');
+					}
 					if (j != n)
 					{
 						_putchar(',');
 						_putchar(' ');
+
+						if (val2 < 100)
+						{
+							_putchar(' ');
+						}
 					}
 				}
 			}
