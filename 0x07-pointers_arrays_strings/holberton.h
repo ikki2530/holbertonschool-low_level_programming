040381ef76e75bbb745c9c82925a58386ee71903
Diego Gómez <dagomez2530@gmail.com>
2020-02-24 14:35:24 +0000

    Add 0-memset.c file

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/* this prototype has to be declared in a header file */
+int _putchar(char c);
+
+/* this prototype has to be declared in a header file */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* _SAMPLE_HEADER_H_ */
