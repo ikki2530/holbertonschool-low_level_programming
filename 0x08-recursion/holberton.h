37ffe796e94a59ccf1c23d97f10aaf4d4a350532
Diego Gómez <dagomez2530@gmail.com>
2020-02-26 16:15:41 +0000

    Add 0-puts_recursion.c fil

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/* this prototype has to be declared in a header file */
+int _putchar(char c);
+
+/* this prototype has to be declared in a header file */
+void _puts_recursion(char *s);
+
+/* this prototype has to be declared in a header file */
+void _print_rev_recursion(char *s);
+
+#endif /* _SAMPLE_HEADER_H_ */
