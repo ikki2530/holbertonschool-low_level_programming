ee971a81826bb4b134dde1acc884eac31eb2458d
Diego Gómez <dagomez2530@gmail.com>
2020-02-26 22:16:24 +0000

    Changes 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
  * val - returns the square root  of a number
- * @n: number
- * Return: square root
+ * @v: val to proof
+ * @num: number to find square root
+ * Return: the square root, or -1 if doesnt have a square root
  */
 int val(int v, int num)
 {
 	int res;
+
 	if (v * v == num)
 		return (v);
 
