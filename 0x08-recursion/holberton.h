f15ff62ebd1f8c729b3fa994ee0127cc7b528a07
Diego Gómez <dagomez2530@gmail.com>
2020-02-26 17:33:07 +0000

    Add 2-strlen_recursion.c file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -10,4 +10,7 @@ void _puts_recursion(char *s);
 /* this prototype has to be declared in a header file */
 void _print_rev_recursion(char *s);
 
+/* this prototype has to be declared in a header file */
+int _strlen_recursion(char *s);
+
 #endif /* _SAMPLE_HEADER_H_ */
