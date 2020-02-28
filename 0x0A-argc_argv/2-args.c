12361a3c2d350119d2fe298019aa8db1e52304cb
Diego Gómez <dagomez2530@gmail.com>
2020-02-28 21:32:33 +0000

    Add 2-args.c file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
- * main - print the number of parameters
+ * main - prints all the arguments
  * @argc: count number of args
  * @argv: array with pointers to each parameter
  * Return: return 0
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char **argv)
 {
-	printf("%i\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
