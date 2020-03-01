19b044c2fc3ee20609f21485ac7cc2813da755a1
Diego Gómez <dagomez2530@gmail.com>
2020-03-01 15:47:15 +0000

    Trust in yourself 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,43 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _isdigit - Checks if a value is a digit
- * @c: var to evaluate
- * Return: 1 if the value is a digit. 0 otherwise
- */
-int _isdigit(char *s)
-{
-	int j, dc = 0, cc = 0;
-
-	for (j = 0; s[j] != '\0'; j++)
-	{
-		if (s[j] >= 48 && s[j] <= 57)
-			dc++;
-		else
-			cc++;
-	}
-
-	printf("valor de j %i\n", j);
-
-	if (dc != 0 && cc == 0)
-		return (atoi(s));
-
-	if (dc != 0 && dc != 0)
-		return (-1);
-
-	if (j == 1 && dc == 0 && cc != 0)
-		return (-1);
-
-	if (dc == 0 && cc != 0)
-		return (0);
-
-	if (dc == 0 && cc == 0)
-		return (0);
-
-	return (1);
-}
-
-/**
  * main - prints all the adding of parameters
  * @argc: count number of args
  * @argv: array with pointers to each parameter
@@ -47,29 +10,33 @@ int _isdigit(char *s)
 int main(int argc, char **argv)
 {
 
-	int i, sum = 0;
-	int num;
+	int i, j, sum = 0, dc = 0, band = 1;
+	char c;
 
 	for (i = 1; i < argc; i++)
 	{
-		num = _isdigit(argv[i]);
-		printf("valor de num int %i, valor de num %i\n", num, num);
-
-		if (num == -1)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (0);
+			c = argv[i][j];
+			if (c <= 48 || c >= 57)
+				band = 0;
+			else
+				dc++;
 		}
-		else if (num == 0)
-		{
-			printf("0\n");
-			return (0);
-		}
+		if (band == 1 && dc > 0)
+			sum += atoi(argv[i]);
 		else
-			sum += num;
+			continue;
 	}
 
-	printf("%i\n", sum);
+	if (dc == 0 && band == 0)
+		printf("%i\n", band);
+	if (dc > 0 && band == 0)
+		printf("Error\n");
+	if (dc == 0 && band == 1)
+		printf("%i\n", dc);
+	if (dc > 0 && band == 1)
+		printf("%i\n", sum);
 
 	return (0);
 }
