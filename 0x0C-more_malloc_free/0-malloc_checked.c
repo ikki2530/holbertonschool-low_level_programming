9a7e5376f4d456da331d95426972c1294f739b1e
Diego Gómez <dagomez2530@gmail.com>
2020-03-05 19:56:32 +0000

    Add 2-calloc.c file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * malloc_checked - allocates memory using malloc
+ * malloc_checked - djaoisjdoias
  * @b: size of the malloc
  * Return: returns a pointer
  */
