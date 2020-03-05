782bf211e1b5b80c24a5a1fb14678101047e52ee
Diego Gómez <dagomez2530@gmail.com>
2020-03-05 21:31:06 +0000

    Changes to 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,69 +5,39 @@
  * string_nconcat - concatenates 2 strings
  * @s1: string to copy
  * @s2: strng to add
+ * @n: numbers of bytes of the s2 string
  * Return: a pointer char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lg, lg2, i, j;
 	char *s;
+	unsigned int lg, lg2, i;
 
-	if (s1 != '\0' && s2 != '\0')
-	{
-		for (lg = 0; s1[lg] != '\0'; lg++)
-		{}
-		for (lg2 = 0; s2[lg2] != '\0'; lg2++)
-		{}
-		if (lg2 >= n)
-		{
-			s = malloc(sizeof(char) * (lg + n + 1));
-			for (i = 0; i < lg; i++)
-				s[i] = s1[i];
-			for (j = 0; j < n; j++)
-				s[lg + j] = s2[j];
-		}
-		else
-		{
-			s = malloc(sizeof(char) * (lg + lg2 + 1));
-			for (i = 0; i < lg; i++)
-				s[i] = s1[i];
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-			for (j = 0; j < lg2; j++)
-				s[lg + j] = s2[j];
-		}
-	}
-	else
+	for (lg = 0; s1[lg] != '\0'; lg++)
+	{}
+
+	for (lg2 = 0; s2[lg2] != '\0'; lg2++)
+	{}
+
+	if (n >= lg2)
+		n = lg2;
+
+	s = malloc(lg + n + 1);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < (lg + n); i++)
 	{
-		if (s1 != '\0' && s2 == '\0')
-		{
-			for (lg = 0; s1[lg] != '\0'; lg++)
-			{}
-			s = malloc(sizeof(char) * (lg + 1));
-			if (s == '\0')
-				return ('\0');
-			for (i = 0; i < lg; i++)
-				s[i] = s1[i];
-		}
-		else if (s1 == '\0' && s2 != '\0')
-		{
-			s = malloc(sizeof(char) * (n + 1));
-			if (s == '\0')
-				return ('\0');
-			for (lg2 = 0; s2[lg2] != '\0'; lg2++)
-			{}
-			if (lg2 >= n)
-			{
-				for (i = 0; i < n; i++)
-					s[i] = s2[i];
-			}
-			else
-			{
-				for (i = 0; i < lg2; i++)
-					s[i] = s2[i];
-			}
-		}
+		if (i < lg)
+			s[i] = s1[i];
 		else
-			s = malloc(0);
+			s[i] = s2[i - lg];
 	}
+
 	return (s);
 }
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,35 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
 
 /**
  * main - check the code for Holberton School students.
@@ -38,10 +9,10 @@ void simple_print_buffer(int *buffer, unsigned int size)
  */
 int main(void)
 {
-	int *a;
+	char *concat;
 
-	a = array_range(0, 10);
-	simple_print_buffer(a, 11);
-	free(a);
+	concat = string_nconcat("Holberton ", "School", 4);
+	printf("%s\n", concat);
+	free(concat);
 	return (0);
 }
