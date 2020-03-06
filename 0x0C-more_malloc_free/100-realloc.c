c81b193200c96d660d08876a2e27d5889ce1cb1a
Diego Gómez <dagomez2530@gmail.com>
2020-03-06 18:12:13 +0000

    Changes to 100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,27 +10,37 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *pt;
+	char *s, *s2;
+	unsigned int i;
 
+	s = ptr;
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (ptr == NULL)
-		pt = malloc(new_size);
-
 	if (new_size == old_size)
 		return (ptr);
 
+	s2 = malloc(new_size);
+	if (s2 == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		return (s2);
+	}
+
 	if (new_size > old_size)
 	{
-		pt = malloc(new_size);
-		if (pt == NULL)
-			return (NULL);
-		pt = ptr;
+		for (i = 0; i < old_size; i++)
+			s2[i] = s[i];
 	}
+	else
+	{
+		for (i = 0; i < new_size; i++)
+			s2[i] = s[i];
+	}
+
 	free(ptr);
-	return (pt);
+	return (s2);
 }
