3541e7a7a7b2de82b0d9604b799e1039eb89cb1c
Diego Gómez <dagomez2530@gmail.com>
2020-03-06 02:51:54 +0000

    Add 100-realloc.c file

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,6 +1,35 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
 
 /**
  * main - check the code for Holberton School students.
@@ -9,10 +38,17 @@
  */
 int main(void)
 {
-	char *concat;
+	char *p;
+	int i;
 
-	concat = string_nconcat("", "School", 4);
-	printf("%s\n", concat);
-	free(concat);
+	p = malloc(sizeof(char) * 10);
+	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+	i = 0;
+	while (i < 98)
+	{
+		p[i++] = 98;
+	}
+	simple_print_buffer(p, 98);
+	free(p);
 	return (0);
 }
