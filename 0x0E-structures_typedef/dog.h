446a5b31ab45d5fa6b35ee016ca2829dbe80b7e8
Diego Gómez <dagomez2530@gmail.com>
2020-03-09 19:52:06 +0000

    Betty changes to the file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG
 #define DOG
-
+/**
+ * struct dog - dog info
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: struct for dog data
+ */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
