9ae4e79212a49b9c35a059b472c95ead8d34e163
Diego Gómez <dagomez2530@gmail.com>
2020-03-11 18:35:41 +0000

    Changes to 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,24 @@
-#include "function_pointers.h"
+#include <stdlib.h>
 #include <stdio.h>
+/**
+ * f - function that prints a name
+ * @name: name passed
+ * Return: nothing
+ */
 void f(char *name)
 {
 	int i;
 
 	for (i = 0; name[i] != '\0'; i++)
-		_putchar(name[i]);
+		printf("%c", name[i]);
 }
+
+/**
+ *print_name -  functions that call the f function
+ *@name: name to pass to f function
+ *@f: function to callback
+ *Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
