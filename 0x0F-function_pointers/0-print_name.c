a9f22c791295279de6b92ef8c308a894f54321b0
Diego Gómez <dagomez2530@gmail.com>
2020-03-11 18:44:35 +0000

    Description in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 /**
- *print_name -  functions that call the f function
- *@name: name to pass to f function
- *@f: function to callback
- *Return: nothing
+ * print_name -  functions that call the f function
+ * @name: name to pass to f function
+ * @f: function to callback
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
