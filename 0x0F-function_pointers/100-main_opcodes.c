984a969a97327f256ccb028fb400c59f60571747
Diego Gómez <dagomez2530@gmail.com>
2020-03-12 00:42:40 +0000

    Adding 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the number of parameters
+ * @argc: count number of args
+ * @argv: array with pointers to each parameter
+ * Return: return 0
+ */
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int bt, i = 0;
+
+	bt = atoi(&*argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (bt < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bt; i++)
+	{
+		printf("%02hhx ", p[i]);
+		if (i == (bt - 1))
+			printf("%02hhx\n", p[i]);
+	}
+
+	return (0);
+}
