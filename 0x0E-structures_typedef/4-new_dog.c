a004ff45ec8c7f2d1e49eae3858ff878e3cddefa
Diego Gómez <dagomez2530@gmail.com>
2020-03-12 18:00:52 +0000

    Deleting a mistake to compile 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,7 +9,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *dg;
+	dog_t *dg;
 	int lg, lg2, i, j;
 
 	dg = malloc(sizeof(dog_t));
@@ -38,6 +38,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		dg->owner[j] = owner[i];
 	dg->name[i] = '\0';
 	dg->owner[j] = '\0';
-	dg->age = age;
+	(*dg).age = age;
 	return (dg);
 }
