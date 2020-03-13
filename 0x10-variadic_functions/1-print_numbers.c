fe8dcab18c4a86217d6b5f68d3833ba55f563351
Diego Gómez <dagomez2530@gmail.com>
2020-03-13 19:48:25 +0000

    Adding 1-print_numbers.c and variadic_functions.h with the corresponding prototype

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints split by a separator
+ *@separator: separator to split the numbers
+ * @n: number of parameters
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list lista;
+
+	if (separator == '\0')
+		return;
+
+	va_start(lista, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i < (n - 1))
+			printf("%d%s", va_arg(lista, int), separator);
+		else
+			printf("%d\n", va_arg(lista, int));
+	}
+	va_end(lista);
+
+}
