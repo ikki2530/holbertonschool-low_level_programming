42568d3ea9c97c15d7b31e1faafa766ea6a89e09
Diego Gómez <dagomez2530@gmail.com>
2020-03-15 16:57:01 +0000

    Description to compare struct in variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,12 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct compare - function use to compare str and call prt function
+ * @str: strign to compare
+ * @prt: function to call
+ * Description: structs with info about a string an its corresponding function
+ */
 typedef struct compare
 {
 	char *str;
