b70cbb6c1027f342204d63f73f5034f844d13f06
Diego Gómez <dagomez2530@gmail.com>
2020-03-19 20:48:16 +0000

    Adding bacis files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_element - prints each element of the list
+ * @h: list element
+ * Return: Nothing
+ */
+void print_element(const list_t *h)
+{
+	if (h->str != '\0')
+		printf("[%u] %s\n", h->len, h->str);
+	else
+		printf("[0] (nil)\n");
+}
+/**
+ * print_list - prints all elements of a list
+ * @h: first element of a list
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h->next != NULL)
+	{
+		print_element(h);
+		h = h->next;
+		i++;
+	}
+	print_element(h);
+	i++;
+	return (i);
+}
