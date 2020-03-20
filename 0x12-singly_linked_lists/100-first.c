8f55ca270eb8dd6e662e030ae1f39fae881bcf67
Diego Gómez <dagomez2530@gmail.com>
2020-03-20 02:32:32 +0000

    Adding 100-first.c lists.h files

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * startupFun - executes before main function
+ * Return: Nothing
+ */
+void startupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
