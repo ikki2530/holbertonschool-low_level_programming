98cf92ad631e1ce8d49a20d1b2cd3963eb90a1bb
Diego Gómez <dagomez2530@gmail.com>
2020-03-24 16:12:27 +0000

    Checking Betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p, *store;
 
-	p = (*head);
-	while (p)
+	if (*head)
 	{
-		store = p->next;
-		free(p);
-		p = store;
+		p = (*head);
+		while (p)
+		{
+			store = p->next;
+			free(p);
+			p = store;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
