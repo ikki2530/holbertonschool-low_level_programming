3c41e6b62a279663933b02c6aa1b9babe7372640
Diego Gómez <dagomez2530@gmail.com>
2020-03-24 17:09:11 +0000

    Checkng Betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,30 +11,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *current;
 	unsigned int cont = 0;
 
-	if (*head != NULL)
+	if (head == NULL)
+		return (NULL);
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
 	{
-		current = *head;
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		if (idx == 0)
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	while (current)
+	{
+		if (cont == (idx - 1))
 		{
-			new->next = *head;
-			*head = new;
-			return (*head);
+			new->next = current->next;
+			current->next = new;
+			return (new);
 		}
-		while (current)
-		{
-			if (cont == (idx - 1))
-			{
-				new->next = current->next;
-				current->next = new;
-				return (new);
-			}
-			cont++;
-			current = current->next;
-		}
+		cont++;
+		current = current->next;
 	}
 	return (NULL);
 }
