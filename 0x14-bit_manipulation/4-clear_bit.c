6f8a821314471b77d98945e042b4523070363da5
Diego Gómez <dagomez2530@gmail.com>
2020-03-27 04:32:28 +0000

    Adding 4-clear_bit.c and adding the prototype to holberton.h

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > ((sizeof(*n) * 8) - 1))
 		return (-1);
 	*n = ~(1 << index) & (*n);
-	printf("New n = %lu\n", *n);
 	return (1);
 }
