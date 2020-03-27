eca0b6a76b2f7ea3e63e0b863dad1ed441f9e949
Diego Gómez <dagomez2530@gmail.com>
2020-03-27 02:13:43 +0000

    Add files 2-get_bit.c and holberton.h

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number
+ * @index: index of the binary number to return
+ * Return: bit at the position index given
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int r, i = 0;
+
+	if (n == 0 && index > 0)
+		return (-1);
+	for (i = 0; n >> i != 0; i++)
+	{
+	}
+	if (index > (i - 1))
+		return (-1);
+
+	r = (n >> index) & 1;
+	return (r);
+}
