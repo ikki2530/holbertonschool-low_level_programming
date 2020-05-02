ce73d5325f750b9afb5e4adc5a4443b4dbaade4d
Diego Gómez <dagomez2530@gmail.com>
2020-05-02 01:19:06 +0000

    Fixing an error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (current)
 	{
-		if (cont == (idx - 1))
+		if (cont == (idx - 1) && current->next != NULL)
 		{
 			new->next = current->next;
 			current->next = new;
