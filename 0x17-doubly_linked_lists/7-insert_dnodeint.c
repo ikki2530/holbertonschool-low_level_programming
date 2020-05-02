4e80520e6800605a58c5547e3b58317997bb3a0d
Diego Gómez <dagomez2530@gmail.com>
2020-05-02 01:26:23 +0000

    Adding NULL condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *current;
 	unsigned int pos = 0;
 
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
