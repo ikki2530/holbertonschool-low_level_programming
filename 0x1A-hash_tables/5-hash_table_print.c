909077c5fa48cbacb8b56327196a80901947bb97
Diego Gómez <dagomez2530@gmail.com>
2020-05-29 06:02:22 +0000

    Fixing Betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,20 +8,23 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int size = 0, i = 0;
+	hash_node_t *current;
 	int flag = 0;
 
 	if (ht == NULL)
 		return;
 	size = ht->size;
+	current = ht->array[i];
 	printf("{");
 	for (i = 0; i < size; i++)
 	{
-		if (ht->array[i] != NULL)
+		while (current != NULL)
 		{
 			if (flag > 0)
 				printf(", ");
 			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
 			flag++;
+			current = current->next;
 		}
 	}
 	printf("}\n");
