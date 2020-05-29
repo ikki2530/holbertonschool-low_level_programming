91f3837ccae23499662911a4786e847e8b61418c
Diego Gómez <dagomez2530@gmail.com>
2020-05-29 05:11:32 +0000

    Fixing Betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,9 +1,13 @@
 #include "hash_tables.h"
-
+/**
+ * free_list - frees a list
+ * @head: first element of a list
+ * Return: Nothing
+ */
 void free_list(hash_node_t *head)
 {
-        hash_node_t *current;
-        hash_node_t *n;
+	hash_node_t *current;
+	hash_node_t *n;
 
 	current = head;
 
@@ -13,28 +17,31 @@ void free_list(hash_node_t *head)
 		{
 			if (current->key)
 				free(current->key);
-                        if (current->value)
-                                free(current->value);
+			if (current->value)
+				free(current->value);
 			n = current->next;
 			free(current);
 			current = n;
 		}
 	}
 }
-
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-        unsigned long int sz, i;
+	unsigned long int sz, i;
 
-        sz = ht->size;
+	sz = ht->size;
 
-        for (i = 0; i < sz; i++)
-        {
-                if (ht->array[i] != NULL)
-                {
-                        free_list(ht->array[i]);
-                }
-        }
-        free(ht->array);
-        free(ht);
+	for (i = 0; i < sz; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			free_list(ht->array[i]);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
