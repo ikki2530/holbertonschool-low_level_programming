a7e20a9d0144846072d954c79d589dfb46091121
Diego Gómez <dagomez2530@gmail.com>
2020-05-29 05:38:20 +0000

    Fixing checks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || key == "")
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
