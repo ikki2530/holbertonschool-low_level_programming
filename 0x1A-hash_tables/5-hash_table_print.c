8a7078b658d72593ed662c89707cc64af25eaff8
Diego Gómez <dagomez2530@gmail.com>
2020-05-29 04:49:27 +0000

    Add all files for mandatory task

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag > 0)
 				printf(", ");
-			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			flag++;
 		}       
 	}
