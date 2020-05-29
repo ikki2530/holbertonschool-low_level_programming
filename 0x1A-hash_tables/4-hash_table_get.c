177948d70bcbba4b3316fb1d54810e1f287faa42
Diego Gómez <dagomez2530@gmail.com>
2020-05-29 05:36:02 +0000

    Fixing checks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,19 +1,28 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key
+ * Return: value associated with the element, or NULL otherwise
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int idx;
-        hash_node_t *current;
+	unsigned long int idx;
+	hash_node_t *current;
 
-        idx = key_index((const unsigned char *)key, ht->size);
-        current = ht->array[idx];
-        while (current)
-        {
-                if (strcmp(current->key, key) == 0)
-                {
-                        return (current->value);
-                }
-        }
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-        return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[idx];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+	}
+
+	return (NULL);
 }
