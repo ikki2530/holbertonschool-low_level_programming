713d76571d38edcc06d2e5a438a85b6c594313e9
Diego Gomez <dagomez2530@gmail.com>
2020-08-25 20:19:01 +0000

    Deleting print function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,4 @@
 #include "search_algos.h"
-
-/**
- * print_array - prints the array from l to r(included)
- * @array: array to be printed
- * @l: left index
- * @r: right index
- * Return: Nothing
- */
-void print_array(int *array, int l, int r)
-{
-	int i = 0;
-
-	printf("Searching in array: ");
-	for (i = l; i <= r; i++)
-	{
-		if (i == r)
-			printf("%d\n", array[i]);
-		else
-			printf("%d, ", array[i]);
-	}
-}
-
-
 /**
  * binary_search - searches for a value in a sorted array of integers
  * @array: pointer to the first element of the array to search in
@@ -31,14 +8,21 @@ void print_array(int *array, int l, int r)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int l = 0, r = (int)size - 1, m = 0;
+	int l = 0, r = (int)size - 1, m = 0, i = 0;
 
 	if (array == NULL)
 		return (-1);
 
 	while (l <= r)
 	{
-		print_array(array, l, r);
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			if (i == r)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
 		m = (l + r) / 2;
 		if (array[m] == value)
 			return (m);
